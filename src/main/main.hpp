c206f9b47e8697c7850609d083da7511d5ee4397
Noel Sollberger <nso107700@iet-gibb.ch>
2021-09-19 22:06:01 +0000

    init

---
+++ src/main/main.hpp
@@ -0,0 +1,14 @@
+/**
+ * @file main.hpp
+ * @author Dr-Zany
+ * @brief 
+ * @version 0.1
+ * @date 2021-09-19
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#pragma once
+
+#include "pico/stdlib.h"
